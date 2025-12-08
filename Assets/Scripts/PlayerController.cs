cf32e63f3f79d3b3edc1f1865390543245a88896
Júlio César <juliocesaroalves679@gmail.com>
2025-12-08 10:35:38 -0300

    Júlio: Adição de animação de movimento e script de movimento horizontal

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float deslocamentoX;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
+    private void Movimentacao()
+    {
+        
+        //Adiciona deslocamento vertical
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            float direita = transform.position.x + deslocamentoX;
+            if (direita > 4.5f) { direita = 4.5f; }
+            transform.position = new Vector2(
+                direita,
+                transform.position.y
+            );
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            float esquerda = transform.position.x - deslocamentoX;
+            if (esquerda < -4.5f){ esquerda = -4.5f; }
+            transform.position = new Vector2(
+                esquerda,
+                transform.position.y
+            );
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        Movimentacao();
     }
 }
